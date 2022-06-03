008ba3f78e73d4b768329dd36c23d55ec49053f9
Ngwsky <ngwsky@gmail.com>
2022-06-03 17:54:57 +0900

    Fix link path probrems @ index.html

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/intermittent-fasting-calculator/',
-  build: {
-    rollupOptions: {
-      external: ['smui.css', 'smui-dark.css'],
-    }
-  },
   plugins: [
     svelte(),
     VitePWA({
